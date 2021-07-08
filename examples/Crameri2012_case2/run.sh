91dbf0684bfe2a4c5fa0005b90e4a021ec0dd25e
Rafael Silva <rafaelmds@users.noreply.github.com>
2021-07-08 15:50:59 -0300

    add missing run (example) script to Crameri example (#8)

---
+++ examples/Crameri2012_case2/run.sh
@@ -0,0 +1,4 @@
+# Please, update the following paths according to your PETSC_DIR and PETSC_ARCH installation
+# and path to MANDYOC binary
+
+${PETSC_DIR}/${PETSC_ARCH}/bin/mpirun -n 4 ../../mandyoc
