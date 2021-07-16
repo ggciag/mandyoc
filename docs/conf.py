7185a9a1a197f1b85270c86925f151c977bba6e8
Agustina Pesce <pesce.agustina@gmail.com>
2021-07-16 17:21:31 -0300

    Update the conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,6 @@ import datetime
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
-
 year = datetime.date.today().year
 project = "MANDYOC"
 copyright = "2020-{}, The {} Developers".format(year, project)
@@ -27,14 +26,10 @@ release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.graphviz",
     "sphinxcontrib.bibtex",
-    # 	'sphinx.ext.autosectionlabel',
+    # 'sphinx.ext.autosectionlabel',
     # 	'sphinx.ext.autodoc',
     # 	'sphinx.ext.intersphinx',
     # 	'sphinx-prompt',
@@ -49,38 +44,49 @@ graphviz_output_format = "svg"
 
 bibtex_bibfiles = ["files/refs.bib"]
 bibtex_default_style = "unsrt"
-
 # bibtex_reference_style = 'author_year'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
+source_suffix = ".rst"
+master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_options = {
+    "logo_only": False,
+    "display_version": True,
+    "prev_next_buttons_location": "bottom",
+    "vcs_pageview_mode": "",
+    # 'style_nav_header_background': 'white',
+    # Toc options
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
+}
 
+html_last_updated_fmt = "%b %d, %Y"
+html_title = project
+html_short_title = project
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+pygments_style = "sphinx"
 
 
 def setup(app):
     app.add_css_file("css/custom.css")
 
-
-pygments_style = "sphinx"
 
 numfig = True
 math_numfig = True
