cba992b5b8a841b25ad6395b33fd17278cef95bf
Victor Sacek <victorsacek@Victors-MacBook-Pro.local>
2021-08-01 07:13:26 +0200

    correction in the script run.sh in the continental rift example

--- examples/continental_rift/run.sh
+++ examples/continental_rift/run.sh
@@ -1,4 +1,4 @@
 /Users/victorsacek/Documents/petsc/arch-label-debug/bin/mpirun -n 8 \
 /Users/victorsacek/Documents/gits/md2d/md2d_aux/mandyoc \
 -seed 0,2 \
--strain_seed 0.0,0.3 \
+-strain_seed 0.0,1.0 \
