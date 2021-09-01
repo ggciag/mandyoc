9eed7c1edfd04a2dd4d762f53673801253fcf25f
Agustina Pesce <pesce.agustina@gmail.com>
2021-09-01 16:47:44 -0300

    Format generate_input_file.py and view_litho_strain_temper.py

--- examples/continental_rift/generate_input_files.py
+++ examples/continental_rift/generate_input_files.py
@@ -1,32 +1,26 @@
+"""
+This example simulates the evolution of divergent margins, taking into account the plastic rheology and the sin-rift geodynamics
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # total model horizontal extent (m)
 Lx = 1600 * 1.0e3
-
 # total model vertical extent (m)
 Lz = 300 * 1.0e3
-
 # number of points in horizontal direction
 Nx = 1601
-
 # number of points in vertical direction
 Nz = 301
-
 # thickness of sticky air layer (m)
 H_sa = 40 * 1.0e3
-
 # thickness of lower crust (m)
 H_lower_crust = 20 * 1.0e3
-
 # thickness of upper crust (m)
 H_upper_crust = 20 * 1.0e3
-
-
 # total thickness of lithosphere (m)
 H_litho = 130 * 1.0e3
-
 # seed depth bellow base of lower crust (m)
 seed_depth = 13 * 1.0e3
 
@@ -39,32 +33,44 @@ X, Z = np.meshgrid(x, z)
 # Interfaces (bottom first)
 ##############################################################################
 interfaces = {
-    'litho': np.ones(Nx) * (H_litho + H_sa),
-    'seed_base': np.ones(Nx) * (seed_depth + H_lower_crust + H_upper_crust + H_sa),
-    'seed_top': np.ones(Nx) * (seed_depth + H_lower_crust + H_upper_crust + H_sa),
-    'lower_crust': np.ones(Nx) * (H_lower_crust + H_upper_crust + H_sa),
-    'upper_crust': np.ones(Nx) * (H_upper_crust + H_sa),
-    'air': np.ones(Nx) * (H_sa),
+    "litho": np.ones(Nx) * (H_litho + H_sa),
+    "seed_base": np.ones(Nx) * (seed_depth + H_lower_crust + H_upper_crust + H_sa),
+    "seed_top": np.ones(Nx) * (seed_depth + H_lower_crust + H_upper_crust + H_sa),
+    "lower_crust": np.ones(Nx) * (H_lower_crust + H_upper_crust + H_sa),
+    "upper_crust": np.ones(Nx) * (H_upper_crust + H_sa),
+    "air": np.ones(Nx) * (H_sa),
 }
 
 # seed thickness (m)
 H_seed = 6 * 1.0e3
-
 # seed horizontal position (m)
 x_seed = 750 * 1.0e3
-
 # seed: number of points of horizontal extent
 n_seed = 2
 
-interfaces['seed_base'][int(Nx*x_seed//Lx - n_seed//2):int(Nx*x_seed//Lx + n_seed//2)] = interfaces['seed_base'][int(Nx*x_seed//Lx - n_seed//2):int(Nx*x_seed//Lx + n_seed//2)] + H_seed//2
-interfaces['seed_top'][int(Nx*x_seed//Lx - n_seed//2):int(Nx*x_seed//Lx + n_seed//2)] = interfaces['seed_top'][int(Nx*x_seed//Lx - n_seed//2):int(Nx*x_seed//Lx + n_seed//2)] - H_seed//2
+interfaces["seed_base"][
+    int(Nx * x_seed // Lx - n_seed // 2) : int(Nx * x_seed // Lx + n_seed // 2)
+] = (
+    interfaces["seed_base"][
+        int(Nx * x_seed // Lx - n_seed // 2) : int(Nx * x_seed // Lx + n_seed // 2)
+    ]
+    + H_seed // 2
+)
+interfaces["seed_top"][
+    int(Nx * x_seed // Lx - n_seed // 2) : int(Nx * x_seed // Lx + n_seed // 2)
+] = (
+    interfaces["seed_top"][
+        int(Nx * x_seed // Lx - n_seed // 2) : int(Nx * x_seed // Lx + n_seed // 2)
+    ]
+    - H_seed // 2
+)
 
+Huc = 2.5e-6 / 2700.0  # old 9.2e-10
+Hlc = 0.8e-6 / 2800.0  # old 2.9e-10
 
-Huc = 2.5E-6/2700.0#old 9.2e-10
-Hlc = 0.8E-6/2800.0#old 2.9e-10
-
-with open('interfaces.txt', 'w') as f:
-    layer_properties = f'''
+# Create the interface file
+with open("interfaces.txt", "w") as f:
+    layer_properties = f"""
         C   1.0       1.0        0.1        1.0        1.0         1.0         1.0
         rho 3378.0    3354.0     3354.0     3354.0     2800.0      2700.0      1.0
         H   0.0       9.0e-12    9.0e-12    9.0e-12    {Hlc}       {Huc}       0.0
@@ -72,28 +78,26 @@ with open('interfaces.txt', 'w') as f:
         n   3.0       3.5        3.5        3.5        4.0         4.0         1.0
         Q   429.0e3   540.0E3    540.0E3    540.0E3    222.0e3     222.0e3     0.0
         V   15.0e-6   25.0e-6    25.0e-6    25.0e-6    0.0         0.0         0.0
-    '''
+    """
 
-    for line in layer_properties.split('\n'):
+    for line in layer_properties.split("\n"):
         line = line.strip()
         if len(line):
-            f.write(' '.join(line.split()) + '\n')
+            f.write(" ".join(line.split()) + "\n")
 
     # layer interfaces
     data = -1 * np.array(tuple(interfaces.values())).T
-    np.savetxt(f, data, fmt='%.1f')
+    np.savetxt(f, data, fmt="%.1f")
 
-
-##############################################################################
 # Plot interfaces
 ##############################################################################
 fig, ax = plt.subplots(figsize=(16, 8))
 
 for label, layer in interfaces.items():
-    ax.plot(x, layer, label=f'{label}')
+    ax.plot(x, layer, label=f"{label}")
 
-ax.set_xticks(np.arange(0, Lx+1, 100 * 1.0e3))
-ax.set_yticks(np.arange(-Lz, 0+1, 10 * 1.0e3))
+ax.set_xticks(np.arange(0, Lx + 1, 100 * 1.0e3))
+ax.set_yticks(np.arange(-Lz, 0 + 1, 10 * 1.0e3))
 
 ax.set_xlim([0, Lx])
 ax.set_ylim([-Lz, 0])
@@ -104,11 +108,10 @@ plt.legend()
 plt.close()
 
 
-
 plt.figure()
 for label, layer in interfaces.items():
-    print(label,":",np.size(layer))
-    plt.plot(x, layer, label=f'{label}')
+    print(label, ":", np.size(layer))
+    plt.plot(x, layer, label=f"{label}")
 ax.set_xlim([0, Lx])
 ax.set_ylim([Lz, 0])
 plt.legend()
@@ -119,7 +122,7 @@ plt.close()
 ##############################################################################
 # Parameters file
 ##############################################################################
-params = f'''
+params = f"""
 nx = {Nx}
 nz = {Nz}
 lx = {Lx}
@@ -228,24 +231,21 @@ right_temperature                   = fixed          # ok
 rheology_model                      = 9             # ok
 T_initial                           = 3             # ok
 
-
-
-
-'''
-
-with open('param.txt', 'w') as f:
-    for line in params.split('\n'):
+"""
+# Create the parameter file
+with open("param.txt", "w") as f:
+    for line in params.split("\n"):
         line = line.strip()
         if len(line):
-            f.write(' '.join(line.split()) + '\n')
+            f.write(" ".join(line.split()) + "\n")
 
 ##############################################################################
 # Initial temperature field
 ##############################################################################
 
-T = 1300 * (z - H_sa) / (H_litho) # Temperatura
+T = 1300 * (z - H_sa) / (H_litho)  # Temperatura
 
-Ta = 1262 / np.exp(-10 * 3.28E-5 * (z - H_sa) / 1250)
+Ta = 1262 / np.exp(-10 * 3.28e-5 * (z - H_sa) / 1250)
 
 T[T < 0.0] = 0.0
 T[T > Ta] = Ta[T > Ta]
@@ -256,21 +256,26 @@ ccapacity = 1250
 
 H = np.zeros_like(T)
 
-cond = (z >= H_sa) & (z < H_upper_crust + H_sa) # upper crust
+cond = (z >= H_sa) & (z < H_upper_crust + H_sa)  # upper crust
 H[cond] = Huc
 
-cond = (z >= H_upper_crust + H_sa) & (z < H_lower_crust + H_upper_crust + H_sa) # lower crust
+cond = (z >= H_upper_crust + H_sa) & (
+    z < H_lower_crust + H_upper_crust + H_sa
+)  # lower crust
 H[cond] = Hlc
 
 Taux = np.copy(T)
 t = 0
 dt = 10000
 dt_sec = dt * 365 * 24 * 3600
-cond = (z>H_sa+H_litho) | (T == 0) #(T > 1300) | (T == 0)
+cond = (z > H_sa + H_litho) | (T == 0)  # (T > 1300) | (T == 0)
 dz = Lz / (Nz - 1)
 
 while t < 500.0e6:
-    T[1:-1] += kappa*dt_sec*((T[2:]+T[:-2]-2*T[1:-1])/dz**2) + H[1:-1]*dt_sec/ccapacity
+    T[1:-1] += (
+        kappa * dt_sec * ((T[2:] + T[:-2] - 2 * T[1:-1]) / dz ** 2)
+        + H[1:-1] * dt_sec / ccapacity
+    )
     T[cond] = Taux[cond]
     t = t + dt
 
@@ -278,33 +283,38 @@ T = np.ones_like(X) * T[:, None]
 
 print(np.shape(T))
 
+# Save the initial temperature file
+np.savetxt("input_temperature_0.txt", np.reshape(T, (Nx * Nz)), header="T1\nT2\nT3\nT4")
 
-np.savetxt('input_temperature_0.txt', np.reshape(T, (Nx*Nz)), header="T1\nT2\nT3\nT4")
-
-
-##############################################################################
 # Plot temperature field and thermal profile
 ##############################################################################
+
 fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, figsize=(16, 8))
 
-ax0.contour(X/1.0e3, (Z-H_sa)/1.0e3, T, levels=np.arange(0, 1610, 100))
-ax0.set_ylim((Lz-H_sa)/1.0e3, -H_sa/1000)
+ax0.contour(X / 1.0e3, (Z - H_sa) / 1.0e3, T, levels=np.arange(0, 1610, 100))
+ax0.set_ylim((Lz - H_sa) / 1.0e3, -H_sa / 1000)
 ax0.set_xlabel("km")
 ax0.set_ylabel("km")
 ax1.set_xlabel("$^\circ$C")
 
-ax1.plot(T[:, 0], (z-H_sa)/1.0e3, '-k')
+ax1.plot(T[:, 0], (z - H_sa) / 1.0e3, "-k")
 
-code=0
+code = 0
 for label in list(interfaces.keys()):
-    code+=1
-    color="C"+str(code)
-    ax1.hlines((interfaces[label][0]-H_sa)/1.0e3, np.min(T[:, 0]), np.max(T[:, 0]), label=f'{label}',color=color)
+    code += 1
+    color = "C" + str(code)
+    ax1.hlines(
+        (interfaces[label][0] - H_sa) / 1.0e3,
+        np.min(T[:, 0]),
+        np.max(T[:, 0]),
+        label=f"{label}",
+        color=color,
+    )
 
-ax1.set_ylim((Lz-H_sa)/1.0e3, -H_sa/1000)
+ax1.set_ylim((Lz - H_sa) / 1.0e3, -H_sa / 1000)
 
 plt.legend()
-plt.savefig('temperature_field.png')
+plt.savefig("temperature_field.png")
 # plt.show()
 plt.close()
 
@@ -316,12 +326,12 @@ plt.close()
 fac_air = 10.0e3
 
 # 1 cm/ano
-vL = 0.005 / (365 * 24 * 3600) # m/s
+vL = 0.005 / (365 * 24 * 3600)  # m/s
 
-h_v_const = 150.0e3 # espessura com velocidade constante
-ha = Lz - H_sa - h_v_const # diferenca
+h_v_const = 150.0e3  # espessura com velocidade constante
+ha = Lz - H_sa - h_v_const  # diferenca
 
-vR = 2 * vL * (h_v_const + ha) / ha # garante que integral seja zero
+vR = 2 * vL * (h_v_const + ha) / ha  # garante que integral seja zero
 
 VX = np.zeros_like(X)
 cond = (Z > h_v_const + H_sa) & (X == 0)
@@ -358,29 +368,28 @@ VZ[cond] = vv
 # print(np.sum(vf))
 # print(np.sum(vf)-np.sum(v0))
 
-VVX = np.copy(np.reshape(VX, Nx*Nz))
-VVZ = np.copy(np.reshape(VZ, Nx*Nz))
+VVX = np.copy(np.reshape(VX, Nx * Nz))
+VVZ = np.copy(np.reshape(VZ, Nx * Nz))
 
-v = np.zeros((2, Nx*Nz))
+v = np.zeros((2, Nx * Nz))
 
 v[0, :] = VVX
 v[1, :] = VVZ
 
 v = np.reshape(v.T, (np.size(v)))
 
+# Create the initial velocity file
 np.savetxt("input_velocity_0.txt", v, header="v1\nv2\nv3\nv4")
 
-
-##############################################################################
 # Plot veolocity
 ##############################################################################
-fig, (ax0,ax1) = plt.subplots(nrows=1, ncols=2, figsize=(9, 9))
+fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, figsize=(9, 9))
 
-ax0.plot(VX[:, 0], (z-H_sa)/1000, 'k-',label = "left side")
-ax1.plot(VZ[:, 0], (z-H_sa)/1000, 'k-',label = "left side")
+ax0.plot(VX[:, 0], (z - H_sa) / 1000, "k-", label="left side")
+ax1.plot(VZ[:, 0], (z - H_sa) / 1000, "k-", label="left side")
 
-ax0.plot(VX[:, -1], (z-H_sa)/1000, 'r-',label = "right side")
-ax1.plot(VZ[:, -1], (z-H_sa)/1000, 'r-',label = "right side")
+ax0.plot(VX[:, -1], (z - H_sa) / 1000, "r-", label="right side")
+ax1.plot(VZ[:, -1], (z - H_sa) / 1000, "r-", label="right side")
 
 ax0.legend()
 ax1.legend()
@@ -388,14 +397,14 @@ ax1.legend()
 ax0_xlim = ax0.get_xlim()
 ax1_xlim = ax1.get_xlim()
 
-ax0.set_yticks(np.arange(0, Lz/1000, 20))
-ax1.set_yticks(np.arange(0, Lz/1000, 20))
+ax0.set_yticks(np.arange(0, Lz / 1000, 20))
+ax1.set_yticks(np.arange(0, Lz / 1000, 20))
 
-ax0.set_ylim([Lz/1000-H_sa/1000, -H_sa/1000])
-ax1.set_ylim([Lz/1000-H_sa/1000, -H_sa/1000])
+ax0.set_ylim([Lz / 1000 - H_sa / 1000, -H_sa / 1000])
+ax1.set_ylim([Lz / 1000 - H_sa / 1000, -H_sa / 1000])
 
-ax0.set_xlim([-8E-10, 8E-10])
-ax1.set_xlim([-8E-10, 8E-10])
+ax0.set_xlim([-8e-10, 8e-10])
+ax1.set_xlim([-8e-10, 8e-10])
 
 ax0.set_xlabel("(m/s)")
 ax0.set_ylabel("Depth (km)")
@@ -404,10 +413,5 @@ ax0.set_title("horizontal component of velocity")
 
 ax1.set_title("vertical component of velocity")
 
-
-plt.savefig('velocity.png')
+plt.savefig("velocity.png")
 plt.close()
-
-
-
-
