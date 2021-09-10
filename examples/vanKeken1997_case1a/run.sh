9e9cd132f39ec30ead669a148834d9ca282fde06
Agustina Pesce <pesce.agustina@gmail.com>
2021-09-10 18:45:52 -0300

    Fix petsc path in vanKeken example

--- examples/vanKeken1997_case1a/run.sh
+++ examples/vanKeken1997_case1a/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-MANDYOC_PATH =../..
-MPI_PATH=$HOME/${PETSC_DIR}/${PETSC_PATH}/bin/
+MPI_PATH=$HOME/petsc/arch-label-optimized/bin
+MANDYOC_PATH=../..
 NUMBER_OF_CORES=2
 
-MPI_PATH/mpirun -n $NUMBER_OF_CORES $MANDYOC_PATH/mandyoc
+$MPI_PATH/mpirun -n $NUMBER_OF_CORES $MANDYOC_PATH/mandyoc
