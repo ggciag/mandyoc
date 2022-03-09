081fff72d6479461504c45075e112c211a1aa36f
Agustina <pesce.agustina@gmail.com>
2022-03-09 14:57:55 -0300

    Create the jupyter notebook for the examples (#58)
    * Create a jupyter notebook to generate and plot the continental rift example
    
    Create the notebook to replace the python scripts.
    
    * Remove readme file.
    This text is in the jupyter notebook now.
    
    * Remove the run.sh script
    
    This code is in the noteook now.
    
    * Update the gitignore file.
    
    Ignore: *.ipynb_*, input files generated by the notebook and the ouput
    files generated after running Mandyoc.
    
    * Improve notebook for continental_rif example.
    
    Improve the plot in postprocessing section.
    
    * Fix labels names un the temperature plot
    
    * Add a notebook to create the input file and plot the resultsfor
    the Crameri example.
    
    Also, remove the old .py files that did this task.
    
    * Create a notebook to plot the comparison between Mandyoc results
    and UNDERWORLD, I2VEA and STAGE results.
    
    Also remove the old .py scripts.
    
    * Remove REDME ans fun files from Crameri example
    
    * Add reference in the notebook to generate the input files for Crameri example
    
    * Create notebook for vanKeken examples and remove old files
    
    The notebook creates and plots the results.
    Remove: README file, run file, .py scripts and density plot.
    
    * Correct or add the axes labels in all the example notebooks
    
    * This fix ordering to plot points in the correct order (time ordered)
    
    Co-authored-by: Rafael Silva <rafaelmds@users.noreply.github.com>
    
    * Fix the typo in the notebook name and update it
    (Crameri notebook/compare_plot_max_surface)
    
    Run the notebook again and add a comment about the order of the mandyoc data.
    
    * Fix typo in a markdown cell in continental rift/create_input_and_plot notebook
    
    Co-authored-by: Rafael Silva <rafaelmds@users.noreply.github.com>

--- examples/Crameri2012_case2/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-MPI_PATH=$HOME/petsc/arch-label-optimized/bin
-MANDYOC_PATH=../..
-NUMBER_OF_CORES=2
-
-$MPI_PATH/mpirun -n $NUMBER_OF_CORES $MANDYOC_PATH/mandyoc
--- examples/continental_rift/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-MPI_PATH=$HOME/petsc/arch-label-optimized/bin
-MANDYOC_PATH=../..
-NUMBER_OF_CORES=2
-
-$MPI_PATH/mpirun -n $NUMBER_OF_CORES $MANDYOC_PATH/mandyoc -seed 0,2 -strain_seed 0.0,1.0 
